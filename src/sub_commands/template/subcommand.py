b3b1e93b54fdf7940932da410250ff604dc07bb1
Zack Farmer <zackfarmer1710@gmail.com>
2024-06-02 13:06:55 -0400

    Added attachments filepath to template create and modify
    - create_template.py
    	- Now passes arguments for an attachment. Using the attachment filepath.
    - subcommand.py
    	- add_subparser()
    		- added -a or --attachment argument.
    	- handle_command()
    		- added namespace.tempalte_attachment so we can read the -a arguments contents
    	- handle_modify()
    		- Added another call to modify_template() if attachment is present.

--- src/sub_commands/template/subcommand.py
+++ src/sub_commands/template/subcommand.py
@@ -13,14 +13,15 @@ class TemplateSubCommand(SubCommand):
 
     def add_subparser(self,sub_parser:_SubParsersAction):
         parser:ArgumentParser = sub_parser.add_parser('template',help='Run to edit, create or modify templates')
-        parser.add_argument('-b','--body',action='store',help='Run to add/modify the body text of a template',dest='template_body',metavar="")
-        parser.add_argument("-n","--name",action='store',help="Run with this to choose a template name",dest="template_name",metavar="")
+        parser.add_argument('-b','--body',action='store',help='Used to set the body text of a template',dest='template_body',metavar="")
+        parser.add_argument("-n","--name",action='store',help="Used to choose the template name to create or modify",dest="template_name",metavar="")
         parser.add_argument('-s','--subject',action='store',help='Used to set or modify the subject of a template',dest='template_subject',metavar="")
+        parser.add_argument('-a','--attachment',action='store',help='Used to set a file path for an attachment within a template',dest='template_attachment',metavar="")
 
         group = parser.add_mutually_exclusive_group(required=True)
-        group.add_argument('-m','--modify',action='store_true',help='Run to choose what template to modify',dest='template_modify')
-        group.add_argument('-c','--create',action='store_true',help='Runs to create a template file',dest='template_create')
-        group.add_argument('-l','--list',action='store_true',help='Run to list all created templates',dest='template_list')
+        group.add_argument('-m','--modify',action='store_true',help='Used to modify a templates configuration. Must be used with one of the setting arguments [-a,-b,-s] to choose what to edit',dest='template_modify')
+        group.add_argument('-c','--create',action='store_true',help='Used to create a new template.',dest='template_create')
+        group.add_argument('-l','--list',action='store_true',help='Used to list all current template names',dest='template_list')
 
 
     def handle_command(self,namespace: Namespace):
@@ -30,18 +31,21 @@ class TemplateSubCommand(SubCommand):
                 raise ArgumentParser.error(ArgumentParser(prog='pyeasymailer'),"Please add -n {template name} argument to specify a template name")
         else:
             if namespace.template_create:
-                self.handle_create(namespace.template_name,namespace.template_subject,namespace.template_body)
+                self.handle_create(namespace.template_name,namespace.template_subject,namespace.template_body,namespace.template_attachment)
             elif namespace.template_modify:
-                self.handle_modify(namespace.template_name,namespace.template_subject,namespace.template_body)
+                self.handle_modify(namespace.template_name,namespace.template_subject,namespace.template_body,namespace.template_attachment)
 
-    def handle_create(self,name,subject,body):
-        create_template(name,subject,body,self.template_path)
+    def handle_create(self,name,subject,body,attachment):
+        create_template(name,subject,body,attachment,self.template_path)
 
-    def handle_modify(self,name,subject,body):
+    def handle_modify(self,name,subject,body,attachment):
         if subject:
             modify_template(name,'subject',subject,self.template_path)
         if body:
             modify_template(name,'body',body,self.template_path)
+        if attachment:
+            modify_template(name,'attachment',attachment,self.template_path)
+
 
     def handle_list(self):
         list_templates()
